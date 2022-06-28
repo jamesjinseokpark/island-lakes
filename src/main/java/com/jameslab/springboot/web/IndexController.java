f00b7d1dd8af6dca307469876050c126f73863bb
jamesjinseokpark <james_711@naver.com>
2022-06-28 17:55:47 +0900

    ch.5-1

--- src/main/java/com/jameslab/springboot/web/IndexController.java
+++ src/main/java/com/jameslab/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jameslab.springboot.web;
 
+import com.jameslab.springboot.config.auth.LoginUser;
+import com.jameslab.springboot.config.auth.dto.SessionUser;
 import com.jameslab.springboot.service.posts.PostsService;
 import com.jameslab.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model)
+    public String index(Model model , @LoginUser SessionUser user)
     {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null)
+        {
+            model.addAttribute("userName" , user.getName());
+        }
 
         return "index";
     }
@@ -24,8 +34,14 @@ public class IndexController {
      * 게시글 추가
      */
     @GetMapping("/posts/save")
-    public String postsSave()
+    public String postsSave(Model model , @LoginUser SessionUser user)
+
     {
+        if(user != null)
+        {
+            model.addAttribute("userName" , user.getName());
+        }
+
         return "posts-save";
     }
 
