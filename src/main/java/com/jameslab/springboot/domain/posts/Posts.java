a9f687927e23056c6de937c3f08b8ed23dd05e07
jamesjinseokpark <james_711@naver.com>
2022-06-28 10:28:06 +0900

    ch.2

---
+++ src/main/java/com/jameslab/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.jameslab.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id // (2)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // (3)
+    private Long id;
+
+    @Column(length = 500, nullable = false) // (4)
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // (7)
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
---
+++ src/main/java/com/jameslab/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.jameslab.springboot.service.posts;
+
+import com.jameslab.springboot.domain.posts.PostsRepository;
+import com.jameslab.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto)
+    {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
